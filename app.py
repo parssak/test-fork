0cb700c723c14f29274f33c727183eae62559aa4
Parssa Kyanzadeh <parssak@gmail.com>
2021-12-23 10:12:53 -0500

    temp removing flask api

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
-from flask import Flask
-from ml import ML
-import asyncio
+# from flask import Flask
+# from ml import ML
+# import asyncio
 
-app = Flask(__name__)
-ml = ML()
-
-
-@app.route("/")
-def hello_world():
-  return "Hello world!"
-
-@app.route("/run")
-def run():
-  # Run the ML program on a separate thread
-  ml.run()
-  return "Running"
-  
+# app = Flask(__name__)
+# ml = ML()
 
 
+# @app.route("/")
+# def hello_world():
+#   return "Hello world!"
+
+# @app.route("/run")
+# def run():
+#   ml.run()
+#   return "Running
